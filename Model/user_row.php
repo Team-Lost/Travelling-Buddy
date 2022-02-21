6ea15bfb29f68a6f9bb380a18843809b993e8dd5
Tasnuva29 <78658046+Tasnuva29@users.noreply.github.com>
2022-02-22 01:30:20 +0600

    DatatTable Approve

---
+++ Model/user_row.php
@@ -0,0 +1,22 @@
+<tr>
+    <td><?php echo $row['UserID']; ?></td>
+    <td><?php echo $row['UserName']; ?></td>
+    <td><?php echo $row['Phone']; ?></td>
+    <td><a href="mailto:"><?php echo $row['Mail']; ?><a></td>
+    <td><?php echo $row['Gender']; ?></td>
+    <td><?php echo $row['Rank']; ?></td>
+    <?php    
+    $fileName = explode('.', $row['IDFile']);
+    if (end($fileName) == "pdf") {
+        $fileDestination = 'UserIdentification/Documents/' . $row['IDFile'];
+    } else {
+        $fileDestination = 'UserIdentification/Images/' . $row['IDFile'];
+    }
+    ?>
+    <td><a href="<?php echo $fileDestination; ?>"><?php echo $row['IDFile']; ?></a></td>
+    <td><?php echo $row['creationDate']; ?></td>
+
+    <td><a href="<?php echo "javascript:Approve($row[UserID])";?>">Approve</a>
+         <a href="<?php echo "javascript:Reject($row[UserID])";?>">Reject</a>
+    </td>
+</tr>
